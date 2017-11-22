64e80901e81d495b062a00bcc6358ef8c8c9bae1
DESKTOP-LH9R10S\Emanuela <emanuela.cybulska@gmail.com>
2017-11-22 23:22:20 +0100

    Adding basic configuration of the project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include "Program.h"
+#include <SDL.h>
+
+int main(int argc, char ** argv)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window * screen = SDL_CreateWindow("My SDL Empty Window",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+
+	SDL_Quit();
+
+	return 0;
+}
